04a22834a39b4001b4a3f5aef9557364e7990225
Viktoria Hillerud <viktoriahillerud@gmail.com>
2023-10-25 16:59:33 +0200

    build

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -24,7 +24,7 @@ import path from "path";
 
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
-	  cb(null, path.join(__dirname, 'images')); // Use an absolute path
+	  cb(null, path.join(__dirname, 'src')); // Use an absolute path
 	},
 	filename: function (req, file, cb) {
 	  const ext = path.extname(file.originalname);
